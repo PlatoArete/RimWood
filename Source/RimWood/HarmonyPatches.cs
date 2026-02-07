4dc0a14cf0b1519b8f6035931c7090cf6ed79c62
PlatoArete <andrew.sproston@gmail.com>
2026-02-07 12:28:59 +0000

    Update Harmony patch for FinalDeteriorationRate method to include additional parameters

--- Source/RimWood/HarmonyPatches.cs
+++ Source/RimWood/HarmonyPatches.cs
@@ -10,17 +10,18 @@ namespace RimWood
     /// Harmony patches for RimWood mod.
     /// Prevents deterioration of items that are actively seasoning.
     /// </summary>
-    [HarmonyPatch(typeof(SteadyEnvironmentEffects), nameof(SteadyEnvironmentEffects.FinalDeteriorationRate),
-                  new Type[] { typeof(Thing), typeof(bool), typeof(bool), typeof(TerrainDef), typeof(List<Thing>) })]
+    [HarmonyPatch(typeof(SteadyEnvironmentEffects), nameof(SteadyEnvironmentEffects.FinalDeteriorationRate))]
+    [HarmonyPatch(new Type[] { typeof(Thing), typeof(List<string>) })]
     public static class SteadyEnvironmentEffects_FinalDeteriorationRate_Patch
     {
         /// <summary>
         /// Postfix patch that cancels deterioration for items actively seasoning.
         /// Logic: If item has CompSeasonable and is actively seasoning (roofed),
         /// set deterioration rate to 0 to prevent HP loss during the seasoning process.
+        /// Targets the 2-parameter public entry point method added in RimWorld 1.6.
         /// </summary>
         [HarmonyPostfix]
-        public static void Postfix(Thing t, ref float __result)
+        public static void Postfix(Thing t, List<string> reasons, ref float __result)
         {
             // Check if thing has seasoning comp
             CompSeasonable comp = t.TryGetComp<CompSeasonable>();
